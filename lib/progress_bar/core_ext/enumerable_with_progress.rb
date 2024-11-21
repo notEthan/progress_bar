6bf6ae2022c6db37377fa9e1da5c5d506d17041e
Ethan <ethan@unth.net>
2024-11-20 22:05:39 -0800

    When core_ext/enumerable_with_progress extends existing modules, skip Refinements
    Refinement#include removed as of Ruby 3.2

--- lib/progress_bar/core_ext/enumerable_with_progress.rb
+++ lib/progress_bar/core_ext/enumerable_with_progress.rb
@@ -3,7 +3,7 @@
 require_relative "../../progress_bar"
 
 ObjectSpace.each_object(Module) do |mod|
-  if mod <= Enumerable
+  if !mod.is_a?(Refinement) && mod <= Enumerable
     mod.send :include, ProgressBar::WithProgress
   end
 end
